c0d45af26b9720652669ded6801ac80f6a68b60a
hanandann <hanandann@gmail.com>
2025-05-25 16:48:51 +0300

    new API key

--- app/src/main/java/com/tinytap/elevenlabsdemo/data/Constants.kt
+++ app/src/main/java/com/tinytap/elevenlabsdemo/data/Constants.kt
@@ -1,5 +1,4 @@
 package com.tinytap.elevenlabsdemo.data
 
-const val API_KEY = "sk_a0e2f567ebb3d25262ef512cd79024d5da720cc4f9fd7663"
-const val AGENT_ID_ROGER = "agent_01jvmfa1h6f4e8fqmt3z82s90m"
+const val API_KEY = "sk_f68905dbd9155e1f91b420e21e353cc28360751e079247c2"
 const val AGENT_ID_EDDIE = "agent_01jvq24xs3fnkapaas177wegn7"
